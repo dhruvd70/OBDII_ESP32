ae636c1bf1d0e9bc882c2825da30c5f99a95bef8
Dhruv Dohare <dhruvd70.dd@gmail.com>
2020-10-02 18:52:35 +0530

    uart initialized

--- main/main.c
+++ main/main.c
@@ -1,42 +1,172 @@
+#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_event.h"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "nvs_flash.h"
+#include "driver/uart.h"
+#include "string.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
+//pin definition for uart_1 and uart_2
+#define TXD_PIN_1 (GPIO_NUM_1)
+#define RXD_PIN_1 (GPIO_NUM_3)
+
+#define TXD_PIN_2 (GPIO_NUM_17)
+#define RXD_PIN_2 (GPIO_NUM_16)
+
+//receiver buffer size definition
+static const int RX_BUF_SIZE_1 = 1024;
+static const int RX_BUF_SIZE_2 = 1024;
+
+// uart_1 for comm. with GPS/GSM
+void uart_1(void)
 {
-    return ESP_OK;
+	const uart_config_t uart_config = {
+			.baud_rate = 115200,   // set baud rate
+			.data_bits = UART_DATA_8_BITS,
+			.parity = UART_PARITY_DISABLE,
+			.stop_bits = UART_STOP_BITS_1,
+			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+			.source_clk = UART_SCLK_APB,
+	};
+
+	uart_driver_install(UART_NUM_1, RX_BUF_SIZE_1 *2, 0, 0, NULL, 0);
+	uart_param_config(UART_NUM_1, &uart_config);
+	uart_set_pin(UART_NUM_1, TXD_PIN_1, RXD_PIN_1, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);  //set uart_1 pins
+
+}
+
+// uart_2 comm. for elm327
+void uart_2(void)
+{
+	const uart_config_t uart_config = {
+			.baud_rate = 38400,   // set baud rate
+			.data_bits = UART_DATA_8_BITS,
+			.parity = UART_PARITY_DISABLE,
+			.stop_bits = UART_STOP_BITS_1,
+			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+			.source_clk = UART_SCLK_APB,
+	};
+
+	uart_driver_install(UART_NUM_2, RX_BUF_SIZE_2 *2, 0, 0, NULL, 0);
+	uart_param_config(UART_NUM_2, &uart_config);
+	uart_set_pin(UART_NUM_2, TXD_PIN_2, RXD_PIN_2, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);  // set uart_2 pins
+
+}
+
+//tx to gps/gsm
+static void tx_task_1(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK_1";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1)
+    {
+    	sendData(TX_TASK_TAG, " ");
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+//rx from gps/gsm
+static void rx_task_1(void *arg)
+{
+	static const char *RX_TASK_TAG = "RX_TASK_1";
+	esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+	uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE_1+1);
+
+	while(1)
+	{
+		const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE_1, 1000 / portTICK_RATE_MS);
+		if(rxBytes > 0)
+		{
+			data[rxBytes] = 0;
+			ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+			ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+
+		}
+	}
+
+	free(data);
+
+}
+
+// tx to elm327
+static void tx_task_2(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK_2";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1)
+    {
+    	sendData(TX_TASK_TAG, " ");
+
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
+
+// rx from elm327
+static void rx_task_2(void *arg)
+{
+	static const char *RX_TASK_TAG = "RX_TASK_2";
+	esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+	uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE_2+1);
+
+	while(1)
+	{
+		const int rxBytes = uart_read_bytes(UART_NUM_2, data, RX_BUF_SIZE_2, 1000 / portTICK_RATE_MS);
+		if(rxBytes > 0)
+		{
+			data[rxBytes] = 0;
+			ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+			ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+
+		}
+	}
+
+	free(data);
+
+}
+
+// function to send data using uart
+int sendData(const char* logName, const char* data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    return txBytes;
 }
 
 void app_main(void)
 {
-    nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+	uart_1();
+	uart_2();
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+	xTaskCreate(tx_task_1, "uart_1_tx_task", 1024*2, NULL, 10, NULL);
+    xTaskCreate(rx_task_1, "uart_1_rx_task", 1024*2, NULL, 10, NULL);
+
+    xTaskCreate(tx_task_2, "uart_1_tx_task", 1024*2, NULL, 10, NULL);
+    xTaskCreate(rx_task_2, "uart_2_rx_task", 1024*2, NULL, 10, NULL);
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
